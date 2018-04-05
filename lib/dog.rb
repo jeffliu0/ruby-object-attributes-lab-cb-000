717ed1113db1356923c0723acb2d27d50b92eedc
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-05 06:08:03 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,5 +5,18 @@ class Dog
   end
 
   def name=(full_name)
+    @name = full_name
+  end
 
+  def name
+    "#{name}"
+  end
+
+  def breed=(type)
+    @breed = type
+  end
+
+  def breed
+    "#{breed}"
+  end
 end
